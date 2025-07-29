c533603d28a0d186a4630266f873b4700bd33f4e
Khoa Nguyen <khoanguyenx@icloud.com>
2025-07-29 16:27:57 +0700

    treesitter

--- nvim/lua/laefhat/plugins/init.lua
+++ nvim/lua/laefhat/plugins/init.lua
@@ -4,6 +4,8 @@ vim.pack.add({
     { src = "https://github.com/mhinz/vim-signify" },
     { src = "https://github.com/junegunn/fzf" },
     { src = "https://github.com/junegunn/fzf.vim" },
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
     { src = "https://github.com/ziglang/zig.vim" },
 })
 require("laefhat.plugins.fuzzy")
+require("laefhat.plugins.treesitter")
